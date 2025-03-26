b7f2f71d29930e44d3baba3fbb1207acccc085af
shreesriv12 <shreeyasrivastava1124@gmail.com>
2025-03-26 16:38:27 +0530

    authform completed

--- finance-management/components/AuthForm.tsx
+++ finance-management/components/AuthForm.tsx
@@ -12,6 +12,8 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { authFormSchema } from "@/lib/utils"; // Use the imported schema
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 // Extended schema to include password validation
 const formSchema = z.object({
@@ -20,10 +22,11 @@ const formSchema = z.object({
 });
 
 const AuthForm = ({ type }: { type: string }) => {
+  const router=useRouter();
     const [isLoading ,setIsLoading]=useState(false);
 
   const [user, setUser] = useState(null);
-
+ const formSchema=authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -32,12 +35,30 @@ const AuthForm = ({ type }: { type: string }) => {
     },
   });
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
 
 
     setIsLoading(true)
-    console.log(values);
-    setIsLoading(false)
+   try{
+    
+
+    if(type==='sign-up'){
+    //  const newUser= await Signup(data);
+    //  setUser(newUser);
+    }
+    if(type==='sign-in'){
+      // const response = await signIn({
+      //     email:data.email,
+      //     password:data.password
+      // })
+      // if(response)
+      //   router.push('/')
+    }
+   }catch(error){
+    console.log(error)
+   }finally{
+    setIsLoading(false);
+   }
   };
 
   return (
@@ -76,13 +97,67 @@ const AuthForm = ({ type }: { type: string }) => {
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              {/* Matching schema field names */}
+              {type==='sign-up' &&(
+                <>
+                <div className="flex gap-4">
+                <CustomInput
+                control={form.control}
+                name="firstName"
+                label="First name"
+                placeholder="Enter your first name"
+              />
+               <CustomInput
+                control={form.control}
+                name="lastName"
+                label="Last Name"
+                placeholder="Enter your last name"
+              />     
+                </div>
+             
               <CustomInput
                 control={form.control}
-                name="email"
-                label="Email"
-                placeholder="Enter your email"
+                name="address1"
+                label="Address"
+                placeholder="Enter your specific address"
               />
+               <CustomInput
+                control={form.control}
+                name="city"
+                label="City"
+                placeholder="Enter your city"
+              />
+
+              <div className="flex gap-4">
+              <CustomInput
+                control={form.control}
+                name="state"
+                label="State"
+                placeholder="Example:Delhi"
+              />
+               <CustomInput
+                control={form.control}
+                name="postalCode"
+                label="Postal Code"
+                placeholder="Example: 11101"
+              />
+              </div>
+              <div className="flex gap-4">
+               <CustomInput
+                control={form.control}
+                name="dateOfBirth"
+                label="Date of Birth"
+                placeholder="YYYY-MM-DD"
+              />
+              <CustomInput
+                control={form.control}
+                name="ssn"
+                label="SSN"
+                placeholder="Example:1234"
+              />
+              </div>
+                </>
+              )}
+              
               <FormMessage />
               <CustomInput
                 control={form.control}
@@ -101,9 +176,30 @@ const AuthForm = ({ type }: { type: string }) => {
                
               <FormMessage />
 
-              <Button className="text-16 rounded-lg border border-bankGradient bg-bank-gradient font-semibold text-white shadow-form" type="submit">Submit</Button>
+              <div className="flex flex-col gap-4">
+              <Button className="text-16 rounded-lg bg-gradient-to-r from-blue-700 to-blue-500  font-semibold text-white shadow-lg p-4" type="submit" disabled={isLoading}>
+                {isLoading?(
+                  <>
+                  <Loader2 size={20}
+                  className="animate-spin"/>&nbsp;
+                  Loading...
+                  </>
+                ):type==='sign-in'
+                ?'Sign In':'Sign Up'}
+              </Button>
+              </div>
             </form>
           </Form>
+          <footer className="flex justify-center gap-1">
+            <p className="text-14 font-normal text-gray-600"
+            >{type==='sign-in'
+            ? "Don't have an account?"
+            : "Already have an account?"}
+            </p>
+            <Link href={type==='sign-in' ? '/sign-up' : '/sign-in'} className="text-14 cursor-pointer font-medium text-blue-500">
+            {type==='sign-in'?'Sign-up':'Sign-in'}
+            </Link>
+            </footer>
         </>
       )}
     </section>
--- finance-management/lib/utils.ts
+++ finance-management/lib/utils.ts
@@ -196,8 +196,18 @@ export const getTransactionStatus = (date: Date) => {
 
 
 
-export const authFormSchema = z.object({
+export const authFormSchema = (type:string)=>z.object({
   email: z.string().email("Invalid email address"),
   password:z.string().min(8),
-  username:z.string().min(8)
+
+
+  firstName:type==='sign-in'? z.string().optional():z.string().min(3),
+lastName:type==='sign-in'? z.string().optional():z.string().min(3),
+address1:type==='sign-in'? z.string().optional():z.string().max(50),
+state:type==='sign-in'? z.string().optional():z.string().min(2).max(2),
+postalCode:type==='sign-in'? z.string().optional():z.string().min(3).max(6),
+dateOfBirth:type==='sign-in'? z.string().optional():z.string().min(3),
+ssn:type==='sign-in'? z.string().optional():z.string().min(3),
+city:type==='sign-in'? z.string().optional():z.string().min(3),
+
 });
